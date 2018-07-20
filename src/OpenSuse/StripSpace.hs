1f41d59633f4bc0f439cb192aaba6b186a538fa7
Peter Simons <simons@cryp.to>
2018-07-20 20:51:35 +0200

    StripSpace.hs: improve the documentation

--- src/OpenSuse/StripSpace.hs
+++ src/OpenSuse/StripSpace.hs
@@ -16,9 +16,9 @@ import qualified Data.Text as Text
 --
 -- * Consecutive empty lines between paragraphs are collapsed into one.
 --
--- * @\r\n@ line endings are normalized into @\n@.
+-- * @\\r\\n@ line endings are normalized into @\\n@.
 --
--- * If the buffer is not empty, then its last line is terminated by @\n@.
+-- * If the buffer is not empty, then its last line is terminated by @\\n@.
 --
 -- * If the buffer is empty (i.e. it contains only white space), then it comes
 --   out as the empty string.
